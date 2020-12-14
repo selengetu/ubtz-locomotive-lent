b2fd05e904583be869d9f91d5f9b556a6a1eef4d
selengetu <49024084+selengetu@users.noreply.github.com>
2020-12-14 16:27:11 +0800

    end

--- resources/views/tailan/machinistnagon.blade.php
+++ resources/views/tailan/machinistnagon.blade.php
@@ -50,7 +50,7 @@
                                     <div class="col-md-3">
                                         <div class="form-group form-md-line-input has-success">
                                             <div class="input-icon">
-                                                <input class="form-control datepicker" id="urtuu30_end" name="urtuu30_end" type="text" value="{{$enddate}}"/>
+                                                <input class="form-control datepicker" id="mach_end" name="mach_end" type="text" value="{{$enddate}}"/>
                                                 <label for="form_control_1" style="font-size:12px;">
                                                     Дуусах огноо
                                                 </label>
@@ -241,7 +241,7 @@
     <script type="text/javascript">
         $(document).ready(function(){
 
-            $("#urtuu30_start").datepicker({
+            $("#mach_start").datepicker({
                 format: 'yyyy-mm-dd',
                 todayBtn:  1,
                 autoclose: true,
@@ -250,7 +250,7 @@
                 $('#urtuu30_end').datepicker('setStartDate', minDate);
             });
 
-            $("#urtuu30_end").datepicker({
+            $("#mach_end").datepicker({
                 format: 'yyyy-mm-dd',
             })
                 .on('changeDate', function (selected) {
