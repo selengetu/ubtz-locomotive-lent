e5556fd6feb060d9099ceb0beec260df483a3038
selengetu <49024084+selengetu@users.noreply.github.com>
2020-12-14 16:57:57 +0800

    test

--- app/Http/Controllers/TailanController.php
+++ app/Http/Controllers/TailanController.php
@@ -3279,7 +3279,7 @@ group by q2.depo_id,q2.marshyear, q2.marshmonth");
 
         }
 
-        $machinist = DB::select("select t.DEPOCODE,t.MASHCODE, t.MASHNAME, sum(SUBSTR(t.patchmin, 1, 2)*60 + SUBSTR(t.patchmin, 4, 2)) as NUHULT
+        dd("select t.DEPOCODE,t.MASHCODE, t.MASHNAME, sum(SUBSTR(t.patchmin, 1, 2)*60 + SUBSTR(t.patchmin, 4, 2)) as NUHULT
                               from v_nagon t 
                               where t.patchmin !='00:00:00' and t.mashname is not null and t.depocode=".Auth::user()->id."  ".$query."
                                group by t.DEPOCODE,t.MASHCODE, t.MASHNAME
